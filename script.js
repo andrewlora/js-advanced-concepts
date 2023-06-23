23674dd45a3b4461f6bea63ca2a1bd4b2e75fc58
Andres Lora <jalc.andres.lora@gmail.com>
2023-06-23 13:29:21 -0700

    added Clousures

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+//////////////////////////////////// Closures //////////////////////////////////////////////////////////////////
+// Two benefits
+// 1: Memory efficient
+
+function heavyDuty(index) {
+  const bigArray = new Array(7000).fill("😃");
+  console.log("created array");
+  return bigArray[index];
+}
+console.log(heavyDuty(20));
+console.log(heavyDuty(200));
+console.log(heavyDuty(2003));
+console.log(heavyDuty(2011));
+
+function heavyDuty2() {
+  const bigArray = new Array(7000).fill("😃");
+  console.log("created array");
+  return function (index) {
+    return bigArray[index];
+  };
+}
+
+const fn2 = heavyDuty2();
+console.log(fn2(20));
+console.log(fn2(200));
+console.log(fn2(2003));
+console.log(fn2(2011));
+
+// 2: Encapsulation
+
+///////////////////////////////////////////////////////////////
+function a() {
+  let grandpa = "grandpa";
+  return function b() {
+    let father = "father";
+    return function c() {
+      let son = "son";
+      return `${grandpa} > ${father} > ${son}`;
+    };
+  };
+}
+
+// console.log(a()()());
+
 //////////////////////////////////// Higher Order Functions  ///////////////////////////
 
 const multiplyBy = (a) => (b) => a * b;
@@ -7,15 +51,15 @@ const multiplyBy = (a) => (b) => a * b;
   };
 };
  */
-console.log("multiplyBy2-4", multiplyBy(2)(4));
-console.log("multiplyBy10-5", multiplyBy(10)(5));
-console.log("multiplyBy100-2", multiplyBy(100)(2));
+// console.log("multiplyBy2-4", multiplyBy(2)(4));
+// console.log("multiplyBy10-5", multiplyBy(10)(5));
+// console.log("multiplyBy100-2", multiplyBy(100)(2));
 const multiplyBy2 = multiplyBy(2);
-console.log("mulby2-4", multiplyBy2(4));
-console.log("mulby2-10", multiplyBy2(10));
+// console.log("mulby2-4", multiplyBy2(4));
+// console.log("mulby2-10", multiplyBy2(10));
 const multiplyBy5 = multiplyBy(5);
-console.log("mulby5-4", multiplyBy5(4));
-console.log("mulby5-10", multiplyBy5(10));
+// console.log("mulby5-4", multiplyBy5(4));
+// console.log("mulby5-10", multiplyBy5(10));
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
