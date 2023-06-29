03ca8b196e1b4264239faa2c15ef45aec493e006
Andres Lora <jalc.andres.lora@gmail.com>
2023-06-29 13:51:47 -0700

    added compose with amazon shopping cart examples

---
+++ amazon.js
@@ -0,0 +1,66 @@
+// Amazon shopping
+
+// Implement a cart feature:
+// 1. Add items to the cart
+// 2. Add 3% tax to item in the cart
+// 3. Buy item: cart --> purchases
+// 4. Empty cart
+
+const user = {
+  name: "Kim",
+  active: true,
+  cart: [],
+  purchases: [],
+};
+
+const amazonHistory = [];
+
+const compose =
+  (f, g) =>
+  (...args) =>
+    f(g(...args));
+
+console.log(
+  "purchaseItem",
+  purchaseItem(
+    emptyCart,
+    buyItem,
+    applyTaxToItems,
+    addItemToCart
+  )(user, { name: "Laptop", price: 344 })
+);
+
+function purchaseItem(...fns) {
+  return fns.reduce(compose);
+}
+
+function addItemToCart(user, item) {
+  amazonHistory.push(user);
+  const updateCart = user.cart.concat(item);
+  return Object.assign({}, user, { cart: updateCart });
+}
+
+function applyTaxToItems(user) {
+  amazonHistory.push(user);
+  const { cart } = user;
+  const taxRate = 1.3;
+  const updatedCart = cart.map((item) => {
+    return {
+      name: item.name,
+      price: item.price * taxRate,
+    };
+  });
+  return Object.assign({}, user, { cart: updatedCart });
+}
+
+function buyItem(user) {
+  amazonHistory.push(user);
+  return Object.assign({}, user, { purchases: user.cart });
+}
+
+function emptyCart(user) {
+  amazonHistory.push(user);
+  return Object.assign({}, user, { cart: [] });
+}
+
+console.log("amazon history", amazonHistory);
